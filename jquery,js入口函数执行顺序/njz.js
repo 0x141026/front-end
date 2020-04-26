65881f22d0e3f6824fe1b002db7d96467992e34f
Nia <sh563295875@163.com>
2020-04-26 22:10:57 +0800

    Update njz.js

--- jquery,js入口函数执行顺序/njz.js
+++ jquery,js入口函数执行顺序/njz.js
@@ -38,6 +38,7 @@ console.log("njz执行和顺序：\n\n"+"立即执行函数(定义多个，依�
 alert("njz执行和顺序：\n"+"立即执行函数(定义多个，依次执行)\n>$(function(){});或者$(document).ready(function(){});(定义多个依次执行)\n"
 			+">window.onload=function(){};(定义多个只执行最后一个)\n>$(window).load(function(){});(定义多个，依次执行)");
 alert("js文件引入的先后顺序是有说法的！！\n后面的js全局变量会覆盖前面的；");
+alert("防止立即执行函数执行不了！！所以记住每种入口函数最后都要加;");
 
 
 
